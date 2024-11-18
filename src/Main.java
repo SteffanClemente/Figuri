773c870eb63a9acf0f911cb03496005dc112ca53
SteffanClemente <steffanclemente@gmail.com>
2024-11-18 10:35:12 +1000

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+    public static void main(String[] args) {
+//        Косая линия 1
+//        int x = 10;
+//        for (int j = 0; j < x; j++) {
+//            for (int i = 0; i < j; i++) {
+//                System.out.print(" ");
+//            }
+//            System.out.println("*");
+//        }
+//        Косая линия 2
+//            int x = 10;
+//            for (int j = 0; j < x; j++) {
+//                for (int i = x - 1; i > j; i--) {
+//                    System.out.print(" ");
+//                }
+//                System.out.println("*");
+//            }
+
+
+//        Прямоугольный треугольник 1
+//            int x = 10;
+//            for (int j = 0; j < x; j++) {
+//                for (int i = 0; i < j; i++) {
+//                    System.out.print("*");
+//                }
+//                System.out.println("*");
+//            }
+//        Прямоугольный треугольник 2
+//            for (int j = 0; j < x; j++) {
+//                for (int i = x - 1; i > j; i--) {
+//                    System.out.print("*");
+//                }
+//                System.out.println("*");
+//            }
+//         Равнобедренный треугольник
+//       for (int i = 0; i < 10; i++) {
+//           for (int j = 9; j > i; j--) {
+//               System.out.print(" ");}
+//           for (int k = -1; k < i * 2; k++) {
+//               System.out.print("*");
+//               }
+//           System.out.println(" ");
+//           }
+//         Ромб
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 9; j > i; j--) {
+//                System.out.print(" ");
+//            }
+//            for (int k = -1; k < i * 2; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+//        for (int j = 0; j < 10; j++) {
+//            for (int i = 0; i < j; i++) {
+//                System.out.print(" ");
+//            }
+//            for (int l = 18; l >j*2 ; l--) {
+//                System.out.print("*");
+//
+//            }
+//            System.out.println("*");
+//        }
+//
+    }
+}
